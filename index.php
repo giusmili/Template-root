562911030b90cc6a4664bd0e1c2159829f018cdc
giusmili <giusmili67@gmail.com>
2024-10-24 15:31:03 +0200

    php

--- index.php
+++ index.php
@@ -14,48 +14,29 @@
 <body>
 
 	<ul>
-		<li><a href="#">études</a></li>
-		<li><a href="">formations</a></li>
-		<li><a href="">contact</a></li>
+		<li><a href="index.php?page=1">études</a></li>
+		<li><a href="index.php?page=2">formations</a></li>
+		<li><a href="index.php?page=3">contact</a></li>
 		
 	</ul>
 	<main>
-		<h1><span aria-hidden="true">🧑‍💻</span> EDW portfolio - développeur</h1>
+		<h1>
+            <span aria-hidden="true">🧑‍💻</span> EDW portfolio - développeur
+        </h1>
+        <?php
+            # code d'appel GET
+
+
+        ?>
 
 	
 			
-<section>
-	<h2>🚀 Présentation</h2>
-		<p>
-			Depuis sa création en 1984, l’EDW a choisi de s’implanter dans des villes créatives et 
-			dynamiques. Présente sur 5 campus en France (Paris, Bordeaux, Aix-en-Provence, Nantes, Lille) 
-			et 6 à l’étranger (dont 4 campus membres du réseau AD Education*), l’EDW est 
-			l’école de référence dans les secteurs du Design, du Digital, de l’Animation et du Jeu vidéo.
-		</p>
-</section>
-<div class="grid-img">
-	<h2>Nos compus en France</h2>
-	<ul>
-		<li><img src=".public/asset/ambiance_01.jpg" alt="Paris"></li>
-		<li><img src=".public/asset/nantes.jpg" alt="Nantes"></li>
-		<li><img src=".public/asset/bordeaux.jpg" alt="Bordeaux"></li>
-	</ul>
-</div>
-<!--icones-->
-	<div class="grid-icone">
-		<h2>Nous suivre</h2>
-		<ul>
-			<li><img src=".public/icones/logo_facebook_3225194.png" alt="FB"></li>
-			<li><img src=".public/icones/logo_twitter_3225183.png" alt="Instagram"></li>
-			<li><img src=".public/icones/logo_instagram_3225191.png" alt="Instagram"></li>
-			<li><img src=".public/icones/logo_linkedin_3225190.png" alt="Instagram"></li>
-		</ul>
-	</div>
+
 
 	</main>
 <footer>
 	<p>&copy; - EDW - 2024<br>
-		<img src=".public/asset/giusmili-lead-developer.svg" alt="badge">
+		<img src="./public/asset/giusmili-lead-developer.svg" alt="badge">
 	</p>
 </footer>
 	
